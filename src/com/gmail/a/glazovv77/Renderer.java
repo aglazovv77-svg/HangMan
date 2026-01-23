7e61bcf2e72121d49402c6e8fa3755bbbd4ec7ba
Aleksey Glazov <a.glazovv77@gmail.com>
2026-01-23 17:45:59 +0300

    added class Renderer

---
+++ src/com/gmail/a/glazovv77/Renderer.java
@@ -0,0 +1,94 @@
+package com.gmail.a.glazovv77;
+
+public class Renderer {
+
+    public static final String[] HANGMAN_STAGES = {
+
+            """
+
+
+
+
+
+					=========
+					""",
+
+            """
+					   |
+					   |
+					   |
+					   |
+					   |
+					=========
+					""",
+
+            """
+					   +---+
+					   |   |
+					   |
+					   |
+					   |
+					=========
+					""",
+
+            """
+					   +---+
+					   |   |
+					   |   O
+					   |
+					   |
+					=========
+					""",
+
+            """
+					   +---+
+					   |   |
+					   |   O
+					   |   |
+					   |
+					=========
+					""",
+
+            """
+					   +---+
+					   |   |
+					   |   O
+					   |  /|
+					   |
+					=========
+					""",
+
+            """
+					   +---+
+					   |   |
+					   |   O
+					   |  /|\\
+					   |
+					=========
+					""",
+
+            """
+					   +---+
+					   |   |
+					   |   O
+					   |  /|\\
+					   |  /
+					=========
+					""",
+
+            """
+					   +---+
+					   |   |
+					   |   O
+					   |  /|\\
+					   |  / \\
+					=========
+					""" };
+
+    public static void render(int stageIndex) {
+        stageIndex = 0;
+        //HANGMAN_STAGES[stageIndex] = HANGMAN_STAGES[stageIndex];
+        System.out.println(HANGMAN_STAGES[stageIndex]);
+    }
+
+}
