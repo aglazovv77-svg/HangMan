ce63bf43a74621b5881eee85f2134816d2fc007a
Aleksey Glazov <a.glazovv77@gmail.com>
2026-01-29 20:58:58 +0300

    частично опменял архитектуру main(), маску слова переделал на String, изменил метод openLetter() открывает тллько букву

--- src/Main.java
+++ src/Main.java
@@ -12,81 +12,79 @@ void main() {
 
             printGreeting();
 
-            String command;
-            do {
-                command = sc.nextLine().toUpperCase();
-                if (command.equals(QUIT)) {
-                    System.out.println("Вы вышли из игры");
-                    return;
-                } else if (!command.equals(START)) {
-                    System.out.println("Вы вводите неверное значение!!!");
-                }
-            } while (!command.equals(START));
-
-            String randomWord = getRandomWord(words);
-
-            char[] masked = "*".repeat(randomWord.length()).toCharArray();
-            System.out.printf("Загаданное слово: %s \n", String.valueOf(masked));
-
-            System.out.printf("У вас %d попыток отгадать слово \n", attemptCount);
-
-            List<Character> errors = new ArrayList<>();
-            int stageIndex = 0;
-
-            while (attemptCount > 0 && String.valueOf(masked).contains("*")) {
-                System.out.println("Введите букву: ");
-                String input = sc.nextLine().toUpperCase();
-
-                Matcher matcher = PATTERN.matcher(input);
-                if (!matcher.matches()) {
-                    System.out.println("Введите ОДНУ РУССКУЮ букву");
-                    continue;
-                }
-
-                char letter = input.charAt(0);
-
-                if (errors.contains(letter) || String.valueOf(masked).indexOf(letter) >= 0) {
-                    System.out.println("Вы уже вводили эту букву");
-                    continue;
-                }
-
-                boolean found = openLetter(randomWord, masked, letter);
-
-                currentWord(masked);
-
-                if (!found) {
-
-                    attemptCount = processError(errors, letter, attemptCount);
-
-                    render(stageIndex);
-                    ++stageIndex;
-
-                    if (attemptCount == 0)
-                        break;
-
-                    System.out.printf("У вас осталось попыток %s \n", attemptCount);
-
-                } else {
-
-                    System.out.printf("Верно! У вас осталось попыток %s \n", attemptCount );
-                    System.out.printf("ошибки: %s \n", errors);
-                }
+            String command = inputCommand();
+            if (isQuit(command)) {
+                System.out.println("Вы вышли из игры");
+                return;
             }
+            start();
 
-            if (!new String(masked).contains("*")) {
-                System.out.println("-----------------------------------");
-                System.out.printf("Поздравляем! Вы выиграли, загаданное слово: %s \n", randomWord);
-            } else {
-                System.out.println("-----------------------------------");
-                System.out.printf("Вы проиграли, загаданное слово: %s \n", randomWord);
-            }
-            System.out.printf("Хотите сыграть еще? \nНажмите [%s], либо любую другую букву для выхода. \n", REPEAT);
-            String answer = sc.nextLine().toUpperCase();
-            if (answer.equals("Y")) {
+        String randomWord = getRandomWord(words);
+
+        String masked = "*".repeat(randomWord.length());
+        System.out.printf("Загаданное слово: %s \n", masked);
+
+        System.out.printf("У вас %d попыток отгадать слово \n", attemptCount);
+
+        List<Character> errors = new ArrayList<>();
+        int stageIndex = 0;
+
+        while (attemptCount > 0 && masked.contains("*")) {
+            System.out.println("Введите букву: ");
+            String input = sc.nextLine().toUpperCase();
+
+            Matcher matcher = PATTERN.matcher(input);
+            if (!matcher.matches()) {
+                System.out.println("Введите ОДНУ РУССКУЮ букву");
                 continue;
             }
-            System.out.println("Игра закончена!");
-            return;
+
+            char letter = input.charAt(0);
+
+            if (errors.contains(letter) || masked.indexOf(letter) >= 0) {
+                System.out.println("Вы уже вводили эту букву");
+                continue;
+            }
+
+            masked = openLetter(randomWord, masked, letter);
+
+            currentWord(masked);
+
+            if (!masked.contains(String.valueOf(letter))) {
+
+                attemptCount = processError(errors, letter, attemptCount);
+
+                render(stageIndex);
+                ++stageIndex;
+
+                if (attemptCount == 0)
+                    break;
+
+                System.out.printf("У вас осталось попыток %s \n", attemptCount);
+
+            } else {
+
+                System.out.printf("Верно! У вас осталось попыток %s \n", attemptCount);
+                System.out.printf("ошибки: %s \n", errors);
+            }
+        }
+
+        if (!masked.contains("*")) {
+            System.out.println("-----------------------------------");
+            System.out.printf("Поздравляем! Вы выиграли, загаданное слово: %s \n", randomWord);
+        } else {
+            System.out.println("-----------------------------------");
+            System.out.printf("Вы проиграли, загаданное слово: %s \n", randomWord);
+        }
+        System.out.printf("Хотите сыграть еще? \nНажмите [%s], либо любую другую букву для выхода. \n", REPEAT);
+        String answer = sc.nextLine().toUpperCase();
+        if (answer.equals("Y")) {
+            continue;
+        }
+        System.out.println("Игра закончена!");
+        return;
         }
     }
 }
+
+
--- src/com/gmail/a/glazovv77/HangMan.java
+++ src/com/gmail/a/glazovv77/HangMan.java
@@ -1,24 +1,43 @@
 package com.gmail.a.glazovv77;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HangMan {
-
+    static Scanner scanner = new Scanner(System.in);
     public final static String START = "N";
     public final static String QUIT = "E";
     public final static String REPEAT = "Y";
     public static final String REGEX = "[А-Яа-яЁё]";
     public static final Pattern PATTERN = Pattern.compile(REGEX);
 
-//    private static String word;
-//    private static char[] masked;
-
     public static void printGreeting() {
         System.out.println("-----------------------------------");
         System.out.printf("Игра ВИСЕЛИЦА \nPush [%s]ew game or [%s]xit \n", START, QUIT);
         System.out.println("-----------------------------------");
+    }
+
+    private static boolean isStart(String command) {
+        return command.equals(START);
+    }
+
+    public static boolean isQuit(String command) {
+        return  command.equals(QUIT);
+    }
+
+    public static String inputCommand() {
+        while(true) {
+            //Scanner scanner = new Scanner(System.in);
+            String command = scanner.nextLine().toUpperCase();
+            if(isStart(command) || isQuit(command)) {
+                return command;
+            }
+            System.out.println("Вы вводите неверное значение!!!");
+        }
     }
 
     public static String getRandomWord(List<String> words) {
@@ -31,19 +50,24 @@ public class HangMan {
         return randomWord;
     }
 
-    public static boolean openLetter(String word, char[] masked, char letter) {
-        boolean found = false;
-        for (int i = 0; i < word.length(); i++) {
-            if (letter == (word.charAt(i))) {
-                masked[i] = letter;
-                found = true;
-            }
-        }
-        return found;
+    public static void start() {
+        //игровая логика раунда
     }
 
-    public static void currentWord(char[] masked) {
-        System.out.printf("Текущее слово: %s \n", String.valueOf(masked));
+    public static String openLetter(String word, String masked, char letter) {
+      char[] maskArray = masked.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
+            if (letter == (word.charAt(i))) {
+                maskArray[i] = letter;
+
+            }
+        }
+        masked = new String(maskArray);
+        return masked;
+    }
+
+    public static void currentWord(String masked) {
+        System.out.printf("Текущее слово: %s \n", masked);
     }
 
     public static int processError(List<Character> errors, char letter, int attemptCount) {
