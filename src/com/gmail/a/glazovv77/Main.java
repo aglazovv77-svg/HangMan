a0b358910e332d0e0774a92491126901937feb4d
Aleksey Glazov <a.glazovv77@gmail.com>
2026-02-02 17:10:34 +0300

    сделал класс Main перетащил класс в пакет

--- src/com/gmail/a/glazovv77/Main.java
+++ src/com/gmail/a/glazovv77/Main.java
@@ -23,175 +23,192 @@ public class Main {
 
     private static final char MASK_SYMBOL = '*';
 
-    private static final String word = getRandomWord(readWords());
-    private static String masked = maskedWord(word);
+    private static String word = getRandomWord(readWords());
+    private static String mask = getMaskWord(word);
+
+    private static List<Character> errorLeters = new ArrayList<>();
 
     public static void main(String[] args) {
 
-    while (true) {
+        while (true) {
 
-        printGreeting();
+            printGreeting();
 
-        String command = inputCommand();
-        if (isQuit(command)) {
-            System.out.println("Вы вышли из игры");
-            return;
-        }
-        start();
-
-       while (true) {
-            System.out.printf("Хотите сыграть еще? \nНажмите [%s], либо любую другую букву для выхода. \n", REPEAT);
-            String answer = scanner.nextLine().toUpperCase();
-
-            if (answer.equals("Y")) {
-                break;
+            String command = inputCommand();
+            if (isQuit(command)) {
+                System.out.println("Вы вышли из игры");
+                return;
             }
-            System.out.println("Игра закончена!");
-            return;
-       }
-    }
-}
+            start();
 
-private static void printGreeting() {
-    System.out.println("-----------------------------------");
-    System.out.printf("Игра ВИСЕЛИЦА \nPush [%s]ew game or [%s]xit \n", START, QUIT);
-    System.out.println("-----------------------------------");
-}
-
-private static boolean isStart(String command) {
-    return command.equals(START);
-}
-
-private static boolean isQuit(String command) {
-    return  command.equals(QUIT);
-}
-
-private static String inputCommand() {
-    while(true) {
-        String command = scanner.nextLine().toUpperCase();
-        if(isStart(command) || isQuit(command)) {
-            return command;
+//            if(needRepeatGame()) {
+//                start();
+//            }
         }
-        System.out.println("Вы вводите неверное значение!!!");
     }
-}
 
-private static String getRandomWord(List<String> words) {
-    Random random = new Random();
-    int indexWord = random.nextInt(words.size());
-    String randomWord = words.get(indexWord).toUpperCase();
+    private static boolean needRepeatGame () {
+        System.out.printf("Хотите сыграть еще? \nНажмите [%s], либо любую другую букву для выхода. \n", REPEAT);
+        String answer = scanner.nextLine().toUpperCase();
 
-    System.out.println(randomWord);  // для отладки
+        return answer.equals("Y");
+    }
 
-    return randomWord;
-}
 
-//игровая логика раунда
-private static void start() {
+    private static void printGreeting() {
+        System.out.println("-----------------------------------");
+        System.out.printf("Игра ВИСЕЛИЦА \nPush [%s]ew game or [%s]xit \n", START, QUIT);
+        System.out.println("-----------------------------------");
+    }
 
-    List<String> words = readWords();
+    private static boolean isStart(String command) {
+        return command.equals(START);
+    }
 
-    int attemptCount = HANGMAN_STAGES.length;
+    private static boolean isQuit(String command) {
+        return command.equals(QUIT);
+    }
 
-    String randomWord = getRandomWord(words);
+    private static String inputCommand() {
+        while (true) {
+            String command = scanner.nextLine().toUpperCase();
+            if (isStart(command) || isQuit(command)) {
+                return command;
+            }
+            System.out.println("Вы вводите неверное значение!!!");
+        }
+    }
 
-    String masked = maskedWord(randomWord);
-    System.out.printf("Загаданное слово: %s \n", masked);
+    private static String getRandomWord(List<String> words) {
+        Random random = new Random();
+        int indexWord = random.nextInt(words.size());
+        String randomWord = words.get(indexWord).toUpperCase();
 
-    System.out.printf("У вас %d попыток отгадать слово \n", attemptCount);
+        System.out.println(randomWord);  // для отладки
 
-    List<Character> errors = new ArrayList<>();
-    int stageIndex = 0;
+        return randomWord;
+    }
 
-    while (!isGameOver()) {
-        System.out.println("Введите букву: ");
-        String input = scanner.nextLine().toUpperCase();
+    //игровая логика раунда
+    private static void start() {
 
-        Matcher matcher = PATTERN.matcher(input);
-        if (!matcher.matches()) {
+        List<String> words = readWords();
+
+        int attemptCount = HANGMAN_STAGES.length;
+
+        word = getRandomWord(words);
+
+        String mask = getMaskWord(word);
+        System.out.printf("Загаданное слово: %s \n", mask);
+
+        System.out.printf("У вас %d попыток отгадать слово \n", attemptCount);
+
+        int stageIndex = 0;
+
+        while (!isGameOver()) {
+
+            char letter = inputRussianLetter();
+
+            if (isUsedLetter(letter)) {
+                System.out.println("Вы уже вводили эту букву");
+                continue;
+            }
+
+            mask = openLetter(word, mask, letter);
+
+            currentWord(mask);
+
+            if (!isWordLetter(letter)) {
+
+                errorLeters.add(letter);
+                attemptCount--;
+                stageIndex++;
+
+//                if (attemptCount == 0) {
+//                    break;
+//                }
+
+                System.out.println("Неверно! Ошибки: " + errorLeters);
+                render(stageIndex);
+                System.out.printf("У вас осталось попыток %s \n", attemptCount);
+
+            } else {
+                System.out.printf("Верно! У вас осталось попыток %s \n", attemptCount);
+                System.out.printf("ошибки: %s \n", errorLeters);
+            }
+
+            if (isWon()) {
+                System.out.println("-----------------------------------");
+                System.out.printf("Поздравляем! Вы выиграли, загаданное слово: %s \n", word);
+            } else if (isLose()) {
+                System.out.println("-----------------------------------");
+                System.out.printf("Вы проиграли, загаданное слово: %s \n", word);
+            }
+        }
+    }
+
+    private static char inputRussianLetter() {
+
+        while (true) {
+            System.out.println("Введите букву: ");
+            String input = scanner.nextLine().toUpperCase();
+
+            Matcher matcher = PATTERN.matcher(input);
+            if (matcher.matches()) {
+                return input.charAt(0);
+            }
             System.out.println("Введите ОДНУ РУССКУЮ букву");
-            continue;
         }
 
-        char letter = input.charAt(0);
-
-        if (errors.contains(letter) || masked.indexOf(letter) >= 0) {
-            System.out.println("Вы уже вводили эту букву");
-            continue;
-        }
-
-        masked = openLetter(randomWord, masked, letter);
-
-        currentWord(masked);
-
-        if (!masked.contains(String.valueOf(letter))) {
-
-            errors.add(letter);
-            System.out.println("Неверно! Ошибки: " + errors);
-
-            --attemptCount;
-
-            render(stageIndex);
-            ++stageIndex;
-
-            if (attemptCount == 0)
-                break;
-
-            System.out.printf("У вас осталось попыток %s \n", attemptCount);
-
-        } else {
-            System.out.printf("Верно! У вас осталось попыток %s \n", attemptCount);
-            System.out.printf("ошибки: %s \n", errors);
-        }
-
-        if (isWon()) {
-            System.out.println("-----------------------------------");
-            System.out.printf("Поздравляем! Вы выиграли, загаданное слово: %s \n", randomWord);
-        }
-        else if (isLose()) {
-            System.out.println("-----------------------------------");
-            System.out.printf("Вы проиграли, загаданное слово: %s \n", randomWord);
-        }
     }
-}
 
-private static String openLetter(String word, String masked, char letter) {
-    char[] maskArray = masked.toCharArray();
-    for (int i = 0; i < word.length(); i++) {
-        if (letter == (word.charAt(i))) {
-            maskArray[i] = letter;
+    private static boolean isUsedLetter(char letter) {
+        return errorLeters.contains(letter) || mask.indexOf(letter) >= 0;
+    }
 
+    private static boolean isWordLetter(char letter) {
+        return word.contains(String.valueOf(letter));
+    }
+
+
+    private static String openLetter(String word, String masked, char letter) {
+        char[] maskArray = masked.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
+            if (letter == (word.charAt(i))) {
+                maskArray[i] = letter;
+
+            }
         }
+        masked = new String(maskArray);
+        return masked;
     }
-    masked = new String(maskArray);
-    return masked;
-}
 
-private static void currentWord(String masked) {
-    System.out.printf("Текущее слово: %s \n", masked);
-}
-
-private static int attemptCount(int attemptCount) {
-    return attemptCount;
-}
-
-private static boolean isGameOver() {
-    return  isWon() || isLose();
-}
-
-private static boolean isLose() {
-    if(attemptCount(HANGMAN_STAGES.length) != 0) {
-        return false;
+    private static void currentWord(String masked) {
+        System.out.printf("Текущее слово: %s \n", masked);
     }
-    return masked.contains(String.valueOf(MASK_SYMBOL));
-}
 
-private static boolean isWon() {
-    return !masked.contains(String.valueOf(MASK_SYMBOL));
-}
+    private static int attemptCount(int attemptCount) {
+        return attemptCount;
+    }
 
-private static String maskedWord(String word) {
+    private static boolean isGameOver() {
+        return isWon() || isLose();
+    }
 
-    return masked = "*".repeat(word.length());
-}
+    private static boolean isLose() {
+        if (attemptCount(HANGMAN_STAGES.length) != 0) {
+            return false;
+        }
+        return mask.contains(String.valueOf(MASK_SYMBOL));
+    }
+
+    private static boolean isWon() {
+        String s = String.valueOf(MASK_SYMBOL);
+        return !mask.contains(s);
+    }
+
+    private static String getMaskWord(String word) {
+
+        return mask = "*".repeat(word.length());
+    }
 }
