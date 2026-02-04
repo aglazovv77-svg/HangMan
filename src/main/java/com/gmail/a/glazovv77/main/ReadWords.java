9ac9fbabc88b210da30eea78031a8190e6612d24
Aleksey Glazov <a.glazovv77@gmail.com>
2026-02-04 11:26:16 +0300

    Remove compiled files from Git

--- src/main/java/com/gmail/a/glazovv77/main/ReadWords.java
+++ src/main/java/com/gmail/a/glazovv77/main/ReadWords.java
@@ -2,16 +2,20 @@ package com.gmail.a.glazovv77.main;
 
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReadWords {
-    public static List<String> readWords()  {
-        String inputFile = "C:\\Users\\aglaz\\ProjectsIdea\\HangMan\\src\\main\\resources\\words.txt";
 
+    private final static String FILE_PATH = "C:\\Users\\aglaz\\ProjectsIdea\\HangMan\\src\\main\\resources\\words.txt";
+
+    public static List<String> readWords()  {
         List<String> words = new ArrayList<>();
+
+        File inputFile = new File(FILE_PATH);
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
             String line;
